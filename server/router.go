3e286029ffae3cfcf1d08d71197950b3480ce5f5
hovanhoa <hovanhoaa1@gmail.com>
2024-09-11 17:41:41 +0700

    feat: health api

--- server/router.go
+++ server/router.go
@@ -1,11 +1,16 @@
 package server
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/hovanhoa/go-url-shortener/internal/handler"
+)
 
 func NewRouter() *gin.Engine {
 	router := gin.New()
 	router.Use(gin.Logger())
 	router.Use(gin.Recovery())
+
+	router.GET("/health", handler.Health)
 
 	return router
 }
