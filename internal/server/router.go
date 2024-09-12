99a90e5fb68d1b7ad26deb10f69319f2f6438c5b
hovanhoa <hovanhoaa1@gmail.com>
2024-09-12 17:43:59 +0700

    feat: add rate limiter

--- internal/server/router.go
+++ internal/server/router.go
@@ -3,16 +3,38 @@ package server
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/hovanhoa/go-url-shortener/internal/handler"
+	"github.com/hovanhoa/go-url-shortener/internal/middleware/ratelimit"
+	"net/http"
+	"time"
 )
+
+func keyFunc(c *gin.Context) string {
+	return c.ClientIP()
+}
+
+func errorHandler(c *gin.Context, info ratelimit.Info) {
+	c.JSON(http.StatusTooManyRequests, gin.H{
+		"err": "Too many requests. Try again in " + time.Until(info.ResetTime).String(),
+	})
+}
 
 func NewRouter(h *handler.Handler) *gin.Engine {
 	router := gin.New()
 	router.Use(gin.Logger())
 	router.Use(gin.Recovery())
 
+	store := ratelimit.InMemoryStore(&ratelimit.InMemoryOptions{
+		Rate:  time.Minute,
+		Limit: 2,
+	})
+	rateLimiter := ratelimit.RateLimiter(store, &ratelimit.Options{
+		ErrorHandler: errorHandler,
+		KeyFunc:      keyFunc,
+	})
+
 	router.GET("/health", handler.Health)
-	router.POST("/sl", h.URLHandler.AddNewURL)
-	router.GET("/sl/:url", h.URLHandler.GetURL)
+	router.POST("/sl", rateLimiter, h.URLHandler.AddNewURL)
+	router.GET("/sl/:url", rateLimiter, h.URLHandler.GetURL)
 
 	return router
 }
