93a826d8c9b3130e57858ee0cda998ed1f30cd9c
hovanhoa <hovanhoaa1@gmail.com>
2024-09-12 12:40:37 +0700

    feat: slient mode for gorm

--- internal/server/server.go
+++ internal/server/server.go
@@ -9,6 +9,7 @@ import (
 	"github.com/hovanhoa/go-url-shortener/pkg/snowflake"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 	"log"
 
 	_ "github.com/lib/pq" // Import the pq driver
@@ -27,7 +28,9 @@ func Init() {
 	)
 
 	// Connect to the PostgreSQL database using GORM
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Silent),
+	})
 	if err != nil {
 		log.Fatal("Failed to connect to the database:", err)
 	}
@@ -36,7 +39,7 @@ func Init() {
 	if err != nil {
 		log.Fatal("Failed to create a snowflake node:", err)
 	}
-	
+
 	s := storage.New(db)
 	svc := service.New(s)
 	h := handler.New(svc, n)
